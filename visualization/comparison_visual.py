caec92965a7a9837ac777c9ca082468433c14cba
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-08-30 09:02:00 -0400

    Adding figures

--- visualization/comparison_visual.py
+++ visualization/comparison_visual.py
@@ -7,7 +7,30 @@ from matplotlib import pyplot as plt
 from matplotlib import colors as colors
 import seaborn as sns
 
-def plot(file_name):
+def plot_loss(file_name, count):
+    raw = pd.read_pickle("est_comparison/"+file_name)
+    optimizer = file_name[file_name.index("_")+1:len(file_name)]
+    optimizer = optimizer[0:optimizer.index("_")]
+    raw['epoch'] = list(range(0, len(raw['loss'])))
+    raw = pd.DataFrame.from_dict(raw)
+    print(raw)
+
+    loss = raw.drop("val_loss", 1)
+    print(loss)
+
+    loss['loss'].rolling(5)
+    loss['loss'] = np.log(loss['loss'])
+
+    with sns.axes_style("darkgrid"):
+        sns.set_palette(sns.color_palette("hls",8))
+        loss = sns.lineplot(x="epoch", y="loss", label=optimizer, data=loss, dashes=True)
+        if not count%2 == 0:
+            loss.lines[count].set_linestyle("--")        
+        loss.set(xlabel='Epoch', ylabel='Loss (log scale)')
+
+    return loss
+
+def plot_valloss(file_name):
     raw = pd.read_pickle("conf_comparison/"+file_name)
     optimizer = file_name[0:file_name.index("_")+1]
     optimizer = optimizer[file_name.index("_"): len(optimizer)]
@@ -15,31 +38,33 @@ def plot(file_name):
     raw = pd.DataFrame.from_dict(raw)
     print(raw)
 
-    loss = raw.drop("val_loss", 1)
     valloss = raw.drop("loss", 1)
 
-    loss['loss'] = np.log(loss['loss'])
     valloss['val_loss'] = np.log(valloss['val_loss'])
-    
-    loss = sns.lineplot(x="epoch", y="loss", label=optimizer+"_train", data=loss)
-    val_loss = sns.lineplot(x="epoch", y="val_loss", label=optimizer+"_validation", data=valloss)
-    loss.set(xlabel='Epoch', ylabel='Loss (log scale)')
+
+    val_loss = sns.lineplot(x="epoch", y="val_loss", label="validation", data=valloss)
     val_loss.set(xlabel='Epoch', ylabel='Loss (log scale)')
 
-    return loss, val_loss
-
+    return val_loss
+    
 def plot_total():
+    count = 0
     loss_total = []
     valloss_total = []
-    for filename in os.listdir("conf_comparison"):
+    optimizers = []
+    for filename in os.listdir("est_comparison"):
         if "model" not in filename:
-            loss, valloss = plot(filename)
+            loss = plot_loss(filename, count)
             loss_total.append(loss)
-            valloss_total.append(valloss)
+            count = count+1
+            #valloss = plot_valloss(filename)
+            #valloss_total.append(valloss)
     
-    for subplot in loss_total:
-        plt.legend()
-        plt.subplot(subplot)
+    print("Size of loss_total: " + str(len(loss_total)))
+    print("Size of valloss_total: " + str(len(valloss_total)))
+
+    for loss_plt in loss_total:
+        plt.subplot(loss_plt)
     plt.show()
     
 if __name__ == '__main__':
